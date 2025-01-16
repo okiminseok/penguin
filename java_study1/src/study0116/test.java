70749948a7d85ff698766608947bf219462e995d
okiminseok <a0596054@gmail.com>
2025-01-16 12:49:59 +0900

    수고하셨습니다

--- java_study1/src/study0116/test.java
+++ java_study1/src/study0116/test.java
@@ -3,8 +3,29 @@ package study0116;
 public class test {
 
 	public static void main(String[] args) {
+		// 주석- 설명이나 메모, 코드 대기 등등
+		/*
+		 * 여러줄 주석 (실행 x) -> 각 코드의 설명 적어놓는 용도로 사용!
+		 *
+		 * 주석 단축키 ctrl+ /
+		 * 실행 단축키 ctrl + f11
+		 *
+		 *
+		 */
 		// TODO Auto-generated method stub
+		System.out.println("자바 이클립스 테스트");
 
+		// 데이터를 사용하려면 변수를 선언해야한다.
+		// 변수를 선언할 때는 반드시 데이터타입을 정해야 한다.
+		// 변수의 이름은 저장할 데이터에 맞는 의미 있는 이름으로 짓기!
+		// 정수 : int, byte, short, long
+		// 실수 : float, double
+		// 문자 : char
+		// 논리 : boolean
+		int age; 
+		age = 100; //변수에 100 저장 (대입연산)
+		age = 27; //같은 공간에 덮어쓰기 한거임;;
+		age = "가"; //자바에서 ''는 문자를 의미! ""해야 .문자열을 의미! (자바스크립트는 '" 모두 문자열!) 문자는 다른 값으로 바뀌어서 저장이 됨. 문자열은 저장xx
 	}
 
 }
